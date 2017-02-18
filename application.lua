728b9d7a0cbc53d79cd28f18d37f901aee5c7de6
Matthias Hryniszak <padcom@gmail.com>
2017-02-18 14:44:20 +0100

    Added RF Switch :)

--- application.lua
+++ application.lua
@@ -5,22 +5,49 @@ password = "e7c5e8247f9da1284ef3480fa7b55a547c22ae74"
 nodeId   = "5e6fbbb0-f0b0-11e6-a971-4db18559717d"
 
 m = mqtt.Client(clientId, 120, username, password, 1)
+m_connected = false
+
+function m_publish(device, data)
+  if m_connected then
+    m:publish("v1/" .. username .."/things/" .. clientId .. "/data/" .. device, data, 0, 0)
+  else
+    print("Not publising - disconnected (" .. device .. " / " .. data .. ")")
+  end
+end
 
 m:on("offline", function(con)
   print("MQTT: disconnected (heap: " .. node.heap() .. ")")
+  m_connected = false
 end)
+
+gniazdko = 0
 
 m:on("message", function(client, topic, data)
   print("MQTT: " .. topic)
   if data ~= nil then
     print("  > " .. data)
   end
+
+  if topic == "v1/" .. username .."/things/" .. clientId .. "/cmd/3" then
+    if gniazdko == 0 then
+      gniazdko = 1
+      rfswitch.send(4, 400, 4, 6, 8305925, 24)
+    else
+      gniazdko = 0
+      rfswitch.send(4, 400, 4, 6, 7952341, 24)
+    end
+    m_publish(3, "" .. gniazdko)
+  end
 end)
 
 m:connect("mqtt.mydevices.com", 1883, 0, 1, function(client)
+  m_connected = true
   print("MQTT: connected");
   m:subscribe("v1/".. username .. "/things/" .. nodeId .. "/cmd/1", 0, function()
-    print("MQTT: subscribe successful") 
+    print("MQTT: subscribe successful")
+  end)
+  m:subscribe("v1/".. username .. "/things/" .. clientId .. "/cmd/3", 0, function()
+    print("MQTT: subscribe successful")
   end)
 end)
 
@@ -30,8 +57,8 @@ bmp085.init(2, 1)
 tmr.create():alarm(5000, tmr.ALARM_AUTO, function()
   local t = bmp085.temperature()
   local p = bmp085.pressure()
-  m:publish("v1/" .. username .."/things/" .. clientId .. "/data/0", "temp,c=" .. string.format("%s.%s", t / 10, t % 10), 0, 0)
-  m:publish("v1/" .. username .."/things/" .. clientId .. "/data/1", "press,hpa=" .. (p/100), 0, 0)
+  m_publish(0, "temp,c=" .. string.format("%s.%s", t / 10, t % 10), 0, 0)
+  m_publish(1, "press,hpa=" .. (p/100), 0, 0)
 end)
 print("BMP180: setup completed")
 
@@ -44,7 +71,7 @@ rotary.on(0, rotary.ALL, function(type, pos, when)
       rotaryUpdateTimer:stop()
     end
     rotaryUpdateTimer:register(100, tmr.ALARM_SINGLE, function()
-      m:publish("v1/" .. username .."/things/" .. clientId .. "/data/2", "temp,c=" .. (pos/4), 0, 0)
+      m_publish(2, "temp,c=" .. (pos/4), 0, 0)
     end)
     rotaryUpdateTimer:start()
   end
