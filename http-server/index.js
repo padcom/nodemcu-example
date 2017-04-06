b690e37f3e95327dc5a3acf976277f163a3dba35
Matthias Hryniszak <padcom@gmail.com>
2017-04-06 10:35:42 +0200

    Added winston for logging in the server

--- http-server/index.js
+++ http-server/index.js
@@ -1,34 +1,53 @@
 #!/usr/bin/env node
 
+const log = require('winston').log;
 const express = require('express')
 const app = express()
 const fs = require('fs')
 const path = require('path')
+const mime = require('mime')
 
-app.get('/', function(req, resp) {
-  resp.send('Hello, world!')
-})
+const devices = {
+  alamakota: {
+    version: '10',
+    files: [ 'application.lua', 'rfrecv.lua' ]
+  }
+}
 
 app.get('/:type', function(req, resp) {
-  console.log(new Date())
-  resp.send({ type: req.params.type, version: '8', files: [ 'application.lua', 'rfrecv.lua' ] })
+  log("info", "GET " + req.params.file)
+
+  if (devices[req.params.type]) {
+    resp.send(devices[req.params.type])
+  } else {
+    resp.writeHead(404)
+    resp.end("Unknow device type")
+  }
 })
 
-app.get('/alamakota/:file', function(req, resp) {
-  console.log("Getting " + req.params.file)
+app.get('/:type/:file', function(req, resp) {
+  if (devices[req.params.type]) {
+    log("info", "GET " + req.params.type + "/" + devices[req.params.type].version + "#" + req.params.file)
+    const filePath = path.join(__dirname, '../' + req.params.file);
 
-  const filePath = path.join(__dirname, '../' + req.params.file);
-  const stat = fs.statSync(filePath);
-
-  resp.writeHead(200, {
-    'Content-Type': 'application/lua',
-    'Content-Length': stat.size
-  });
-
-  fs.createReadStream(filePath).pipe(resp);
+    if (fs.existsSync(filePath)) {
+      const stat = fs.statSync(filePath);
+      resp.writeHead(200, {
+        'Content-Type': mime.lookup(req.params.file),
+        'Content-Length': stat.size
+      });
+      fs.createReadStream(filePath).pipe(resp);
+    } else {
+      resp.writeHead(404)
+      resp.end("File not found")
+    }
+  } else {
+    resp.writeHead(404)
+    resp.end("Unknown device type")
+  }
 })
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  log("info", 'Example app listening on port 3000!')
 })
