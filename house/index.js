f6f6a4c0187d77d80a94cfa2479569c72f29bf93
Matthias Hryniszak <padcom@gmail.com>
2017-04-23 14:12:14 +0200

    Added log and fixed problem on iPhone

--- house/index.js
+++ house/index.js
@@ -5,8 +5,18 @@ console.log("Hello")
 const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://192.168.32.2', { clientId: 'house' })
 
+client.subscribe('bus/rf/433/in')
+client.subscribe('bus/rf/433/out')
+
+client.on('message', function(topic, message) {
+  if (topic == 'bus/rf/433/in') handleRfInMessage(message.toString());
+  if (topic == 'bus/rf/433/out') handleRfOutMessage(message.toString());
+});
+
+
 const express = require('express')
 const app = express()
+const expressWs = require('express-ws')(app)
 
 app.use(express.static('public'))
 app.use(require('body-parser').json())
@@ -37,6 +47,29 @@ app.post('/api/switch/:switch', function(req, res) {
     res.status(404).send("Switch not found")
   }
 })
+
+app.ws('/echo', function(ws, req) {
+  ws.on('message', function(msg) {
+    ws.send(msg);
+  });
+});
+
+function broadcast(clients, message) {
+  clients.forEach(function(ws) {
+    ws.send(message);
+  });
+}
+
+var eventsWs = expressWs.getWss('/events');
+
+function handleRfOutMessage(message) {
+  broadcast(eventsWs.clients, message)
+}
+
+function handleRfInMessage(message) {
+  broadcast(eventsWs.clients, message)
+}
+
 
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!')
